e2c0fb597f8c8a467f8a868dad80435f961dbcd4
KeysiBruno <bruno.arroyo06kc@gmail.com>
2025-02-16 13:17:16 -0700

    "Lab 2 update 4"

--- Labs/Lab2/point.cpp
+++ Labs/Lab2/point.cpp
@@ -1,54 +1,66 @@
 #include "point.h"
 
 //Constructor
-points::Point::Point(int X, int Y, points::Point* closestPoint)
+points::Point::Point(int X, int Y, points::Point* closestPoint) : x(X), y(Y), nearestPoint(closestPoint)
 {
-
 }
 //Destructor
 points::Point::~Point()
 {
-
 }
 
 //Getters
 int points::Point::getX()
 {
-    return 0;
+    return x;
 }
 
 int points::Point::getY()
 {
-    return 0;
+    return y;
 }
 
 points::Point* points::Point::getNearestPoint()
 {
-    return nullptr;
+    return nearestPoint;
 }
 
 points::Point* points::Point::calcNearestPoint(Point* pointList[], unsigned long arrSize)
-{
+{  
+     double fake_distance = 99999;
+     double _distance;
+     Point *Nearest_Point;
+
+      while(arrSize > 0)
+      {
+         for(int i=0; i<arrSize; i++)
+         {
+         }
+      }
+
+
     return nullptr;
 }
 
 //Setters
 void points::Point::setX(int newVal)
 {
-
+    x = newVal;
 }
 
 void points::Point::setY(int newVal)
 {
-
+   y = newVal;
 }
 
 void points::Point::setNearestPoint(Point* newNearestPoint)
 {
-
+   nearestPoint = newNearestPoint;
 }
 
 double points::Point::distPoints(Point& point)
 {
-    return 0.0;
+    double distance;
+    distance = sqrt(pow(x-point.getX(), 2) + pow(y-point.getY(), 2));
+    return distance;
 }
