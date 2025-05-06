06f215cc548f6127f622d79d76da1de829c6f96c
KeysiBruno <bruno.arroyo06kc@gmail.com>
2025-05-05 20:24:24 -0600

    "Last change"

--- Assignments/HW4/main.cpp
+++ Assignments/HW4/main.cpp
@@ -2,15 +2,92 @@
 #include <string>
 #include <sstream>
 #include "stack.h"
+#include <cassert>
 
 using namespace std;
 
-double evaluateRPN(string expression)
-{
-    return 0.0;
-}
+void tests();
+int evaluateRPN(string);
 
 int main(int argc, char *argv[])
 {
-    return 0;
+    if (argc == 3 && string(argv[1]) == "-p") {
+        string input = argv[2];
+        int result = evaluateRPN(input);
+        cout << "Result: " << result << endl;
+    } else {
+        tests();
+    }
+  return 0;
+}
+
+int evaluateRPN(string expression)
+{
+    int a, b, result;
+    Stack s1;
+   //evaluate if its a digit
+   //evaluate if its an operator
+   //evaluate if its a useful expression
+   for(char c : expression)
+   {
+    if(isdigit(c))
+     {
+        int num = c - '0';
+        s1.push(num);
+     }
+
+     else if(c=='+' || c=='-' || c=='*' || c=='/')
+     {
+        if(s1.size() < 2)
+        {
+            cout<<"There are no numbers to make an operation. "<<endl;
+            return NAN;
+        }
+        a =  s1.top();
+        s1.pop();
+        b = s1.top();
+        s1.pop();
+
+        if(c=='+')
+        {
+           // cout<<"DEBUG: b is: "<<b<<"  and a is: "<<a<<endl;
+            result = b + a;
+        }
+        else if(c=='-')
+        {
+            //cout<<"DEBUG: b is: "<<b<<"  and a is: "<<a<<endl;
+            result = b - a;
+        }
+        else if(c=='*')
+        {
+            //cout<<"DEBUG: b is: "<<b<<"  and a is: "<<a<<endl;
+            result = b * a;
+        }
+        else if(c=='/')
+        {
+            //cout<<"DEBUG: b is: "<<b<<"  and a is: "<<a<<endl;
+            if(a == 0) return NAN;
+            result = b / a;
+        }
+        s1.push(result);  
+      }
+      else
+      {
+        cout<<"Invalid expression. "<<endl;
+        return NAN;
+      }
+   }
+   return result;
+}
+void tests()
+{
+    assert(evaluateRPN("23+") == 5);       // 2 + 3 = 5
+    assert(evaluateRPN("234-+") == 1);     // 3 - 4 = -1, 2 + (-1) = 1
+    assert(evaluateRPN("23-4+") == 3);     // 2 - 3 = -1, -1 + 4 = 3
+    assert(evaluateRPN("23*4+") == 10);    // 2 * 3 = 6, 6 + 4 = 10
+    assert(evaluateRPN("92/") == 4);       // 9 / 2 = 4 
+    assert(evaluateRPN("52/3+") == 5);     // 5 / 2 = 2, 2 + 3 = 5
+    assert(evaluateRPN("34+56-*") == -7);  // (3 + 4) * (5 - 6) = 7 * -1 = -7
+
+    cout<<"All tests passed. "<<endl;
 }
--- Assignments/HW4/stack.h
+++ Assignments/HW4/stack.h
@@ -24,39 +24,75 @@ class Stack
 };
 
 // set to nullptr and initialize stackSize
-Stack::Stack()
+Stack::Stack() : _top(nullptr), stackSize(0)
 {
 }
 
 // iteratively delete the stack starting at top
 Stack::~Stack()
 {
+    if(stackSize==0)
+    {
+        cout<<"Empty stack. "<<endl;
+        return;
+    }
+    while(_top != nullptr)
+    {
+        Node<double> *delete_stack=_top;
+        _top = _top->getNext();
+        delete delete_stack;
+    }
 }
 
 // return true if the Stack is empty, false otherwise.
 // Do not just check stackSize, should actually check top
 bool Stack::empty()
 {
+    return (_top==nullptr);
 }
 
 // return number of elements in Stack
 size_t Stack::size()
 {
+    return stackSize;
 }
 
 // add an element to the beginning of the Stack, updating top
 void Stack::push(double data)
 {
+    Node<double> *new_node = new Node<double>();
+    new_node->setData(data);
+    new_node->setNext(_top);
+    _top=new_node;
+
+    stackSize++;
 }
 
 // return the first element in the Stack.
 // if the Stack is empty, print an error and return NaN (from cmath)
 double Stack::top()
 {
+    if(_top==nullptr)
+    {
+        cout<<"Error. "<<endl;
+        return NAN;
+    }
+    return _top->getData();
 }
 
 // remove the first element from the Stack and return its data
 // if the Stack is empty, print an error and return NaN (from cmath)
 double Stack::pop()
 {
-}
+    Node<double> *first = _top;
+    double first_element = first->getData();
+   
+    if(_top==nullptr)
+    {
+        cout<<"Error. "<<endl;
+        return NAN;
+    }
+    _top=_top->getNext();
+    delete first;
+    return first_element;
+} 
